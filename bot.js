5c2e6edbdfe72bed59650719b9c3c308329ebe47
outadoc <outadoc@gmail.com>
2013-04-03 11:29:13 +0200

    Move logging functions to its own module

--- bot.js
+++ bot.js
@@ -3,8 +3,10 @@
 */
 
 (function() {
-		//the twitter api module
-		var twitter = require('ntwitter'),
+	//the twitter api module
+	var twitter = require('ntwitter'),
+		LogUtils = require('./lib/LogUtils.js'),
+
 		//the username of the bot. not set to begin with, we'll get it when authenticating
 		botUsername = null,
 
@@ -20,16 +22,16 @@
 	twitterAPI.verifyCredentials(function(error, userdata) {
 		if (error) {
 			//if we don't, we'd better stop here anyway
-			logtrace(error);
+			LogUtils.logtrace(error, LogUtils.Colors.RED);
 		} else {
 			//the credentials check returns the username, so we can store it here
 			botUsername = userdata.screen_name;
-			logtrace("logged in as " + userdata.screen_name);
+			LogUtils.logtrace("logged in as " + userdata.screen_name, LogUtils.Colors.BLUE);
 
 			//start listening to tweets that contain the bot's username using the streaming api
 			twitterAPI.stream('user', { with:'followings', track:'@' + botUsername },
 				function(stream) {
-					logtrace("streaming");
+					LogUtils.logtrace("streaming", LogUtils.Colors.BLUE);
 
 					//when we're receiving something
 					stream.on('data', function(data) {
@@ -38,7 +40,7 @@
 							&& data.text.toLowerCase().indexOf('@' + botUsername.toLowerCase()) != -1 	//if it's really mentionning us (it should)
 							&& data.retweeted_status === undefined) {									//and if it isn't a retweet of one of our tweets
 
-							logtrace("@mention from " + data.user.screen_name);
+							LogUtils.logtrace("@mention from " + data.user.screen_name, LogUtils.Colors.GREEN);
 							
 							//getting a random tweet using the "yes.thatcan.be/my/next/tweet/" method							
 							//we pass it the username of the real person, a reference to the twitter api module, and a callback
@@ -46,9 +48,9 @@
 								function(error, newTweetData) {
 									if (error) {
 										//handling the error, again
-										logtrace(error);
+										LogUtils.logtrace(error, LogUtils.Colors.RED);
 									} else {
-										logtrace("#got random tweet for " + newTweetData.username);
+										LogUtils.logtrace("#got random tweet for " + newTweetData.username, LogUtils.Colors.GREEN);
 										//store the final tweet (containing the mention)
 										var tweetDone = '@' + newTweetData.username + " " + newTweetData.tweet;
 										
@@ -56,9 +58,9 @@
 										twitterAPI.updateStatus(tweetDone.substring(0, 139), { in_reply_to_status_id: newTweetData.reply_id },
 											function(error, statusData) {
 												if (error) {
-													logtrace(error);
+													LogUtils.logtrace(error, LogUtils.Colors.RED);
 												} else {
-													logtrace("->replied to " + statusData.in_reply_to_screen_name);
+													LogUtils.logtrace("->replied to " + statusData.in_reply_to_screen_name, LogUtils.Colors.GREEN);
 												}
 											}
 										);
@@ -70,48 +72,16 @@
 
 					stream.on('end', function(e) {
 						//if it stops, log it
-						logtrace("STREAM STOPPED. (" + e + ")");
+						LogUtils.logtrace("STREAM STOPPED. (" + e + ")", LogUtils.Colors.RED);
 					});
 					
 					stream.on('error', function (e, code) {
 						//if it encounters an error... well, fuck.
-						logtrace("STREAM ERROR. (" + e + " " + code + ")");
+						LogUtils.logtrace("STREAM ERROR. (" + e + " " + code + ")", LogUtils.Colors.RED);
 					});
 				}
 			);
 		}
 	});
-	
-	function logtrace(message, color) {
-		var messageColor;
-
-		if(color == "red") {
-			messageColor = '\033[31m';
-		} else if(color == "green") {
-			messageColor = '\033[32m';
-		} else if(color == "yellow") {
-			messageColor = '\033[33m';
-		} else if(color == "blue") {
-			messageColor = '\033[34m';
-		} else {
-			messageColor = '\033[0m';
-		}
-
-		//just log the message with a timestamp
-		console.log('\033[33m' + getTimestampString() + messageColor + message);
-	}
-
-	function getTimestampString() {
-		var date = new Date();
-		//format nicely a timestamp we can put in front of our logs
-		return "[" + addUselessZero(date.getDate()) + "/" + addUselessZero(date.getMonth()+1) + "/" + addUselessZero(date.getFullYear()) + "-" + addUselessZero(date.getHours()) + ":" + addUselessZero(date.getMinutes()) + ":" + addUselessZero(date.getSeconds()) + "] ";
-	}
-
-	function addUselessZero(num) {
-		if(num < 10) {
-			return '0' + num.toString();
-		}
-		return num;
-	}
 
 })();
